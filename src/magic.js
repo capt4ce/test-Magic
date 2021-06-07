7ee648facf7f141070f9d6e3cc2803210362ad41
Ahmad Ali Abdilah <ahmad.ali.abdilah@gmail.com>
2021-06-07 13:47:06 +0800

    testing with test key and live key

--- src/magic.js
+++ src/magic.js
@@ -1,23 +1,16 @@
 import { Magic } from 'magic-sdk';
 import Web3 from 'web3';
 
-/** 
- * NOTE: when connecting to a testnet, TEST API keys must be used from the Magic dashboard (live API keys for eth mainnet)
- */
-
-const customNodeOptions = {
-  rpcUrl: 'https://rpc-mumbai.matic.today',
-  chainId: 80001
-}
-
-// Setting network to Matic
-export const magicMatic = new Magic(process.env.REACT_APP_MAGIC_PUBLISHABLE_KEY, { network: customNodeOptions });
-magicMatic.network = "matic"
+// Setting network to Rinkeby
+export const magicMatic = new Magic(REACT_APP_MAGIC_PUBLISHABLE_KEY_TEST, {
+  network: 'rinkeby',
+});
+magicMatic.network = 'rinkeby';
 
 export const maticWeb3 = new Web3(magicMatic.rpcProvider);
 
-// Setting network to Ethereum (Ropsten Testnet)
-export const magicEthereum = new Magic(process.env.REACT_APP_MAGIC_PUBLISHABLE_KEY, { network: 'ropsten' });
-magicEthereum.network = "ethereum"
+// Setting network to Ethereum
+export const magicEthereum = new Magic(REACT_APP_MAGIC_PUBLISHABLE_KEY);
+magicEthereum.network = 'ethereum';
 
 export const ethWeb3 = new Web3(magicEthereum.rpcProvider);
